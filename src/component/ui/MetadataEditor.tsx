086a0e5ac923f791660c75cf2fdeaa6285252684
Bernard Llanos <bernard@mojow.ai>
2021-01-13 11:48:14 -0700

    Fix: set dirty metadata state in a useEffect call

--- src/component/ui/MetadataEditor.tsx
+++ src/component/ui/MetadataEditor.tsx
@@ -10,7 +10,11 @@ import { StoreContext } from '../../state/StoreContext';
 import { MetadataFieldList } from './MetadataForm';
 import { useMetadata } from '../../hooks/useMetadata';
 import { MetadataInteraction } from '../../type/metadata';
-import type { MetadataFormInitialValues } from '../../type/metadata';
+import type {
+  Metadata,
+  MetadataFormStarter,
+  MetadataFormInitialValues,
+} from '../../type/metadata';
 
 /**
  * @ignore
@@ -26,6 +30,97 @@ const styles = StyleSheet.create({
     height: '10%',
   },
 });
+
+/**
+ * A component that is passed to Formik to render the inside of the metadata editing form.
+ * @param props Rendering props
+ */
+function _FormContents({
+  formik,
+  extra,
+}: {
+  /**
+   * Formik-provided data and helpers
+   */
+  formik: FormikProps<MetadataFormInitialValues>;
+  /**
+   * Contextual data provided by [[MetadataEditor]]
+   */
+  extra: {
+    formStarter: MetadataFormStarter;
+    data: Metadata | null | undefined;
+    use: MetadataInteraction.Create | MetadataInteraction.Edit;
+    isEditOperation: boolean;
+  };
+}) {
+  /**
+   * Unpack rendering props
+   */
+  const { dirty, isSubmitting, isValid, submitForm } = formik;
+  const { formStarter, data, use, isEditOperation } = extra;
+
+  const { controls } = useContext(StoreContext);
+
+  /**
+   * Function to notify the controller of dirty form state
+   */
+  const setDirty = useMemo(
+    () =>
+      action('metadata_editor_dirty', (dirty: boolean) => {
+        if (dirty) {
+          /**
+           * Notify the controller that there is dirty state.
+           * The controller will warn the user about unsaved changes.
+           */
+          controls.dirtyMetadata = {};
+        } else {
+          controls.dirtyMetadata = null;
+        }
+      }),
+    [controls]
+  );
+
+  useEffect(() => {
+    setDirty(dirty);
+  }, [dirty, setDirty]);
+
+  /**
+   * Cancel button callback
+   */
+  const onDismiss = useMemo(
+    () =>
+      action('metadata_editor_cancel', () => {
+        controls.cancel();
+      }),
+    [controls]
+  );
+
+  return (
+    <>
+      <Card.Content style={styles.cardContent}>
+        <MetadataFieldList
+          formFieldList={formStarter.formStructure.fields}
+          use={use}
+          data={data}
+        />
+      </Card.Content>
+      <Card.Actions style={styles.cardActions}>
+        <Button
+          onPress={submitForm}
+          disabled={!isValid || isSubmitting || (isEditOperation && !dirty)}
+        >
+          Save
+        </Button>
+        <Button onPress={onDismiss}>Cancel</Button>
+      </Card.Actions>
+    </>
+  );
+}
+
+/**
+ * Renderable MobX wrapper for [[_FormContents]]
+ */
+export const FormContents = observer(_FormContents);
 
 /**
  * A component that renders an interface for editing geometry metadata
@@ -95,69 +190,17 @@ function _MetadataEditor() {
   );
 
   /**
-   * Cancel button callback
-   */
-  const onDismiss = useMemo(
-    () =>
-      action('metadata_editor_cancel', () => {
-        controls.cancel();
-      }),
-    [controls]
-  );
-
-  /**
-   * Function to notify the controller of dirty form state
-   */
-  const setDirty = useMemo(
-    () =>
-      action('metadata_editor_dirty', (dirty: boolean) => {
-        if (dirty) {
-          /**
-           * Notify the controller that there is dirty state.
-           * The controller will warn the user about unsaved changes.
-           */
-          controls.dirtyMetadata = {};
-        } else {
-          controls.dirtyMetadata = null;
-        }
-      }),
-    [controls]
-  );
-
-  /**
    * The body of the Formik form, which renders a list of form fields
    * and submit or cancel buttons.
    */
   const formContents = useCallback(
-    ({
-      dirty,
-      isSubmitting,
-      isValid,
-      submitForm,
-    }: FormikProps<MetadataFormInitialValues>) => {
-      setDirty(dirty);
-      return (
-        <>
-          <Card.Content style={styles.cardContent}>
-            <MetadataFieldList
-              formFieldList={formStarter.formStructure.fields}
-              use={use}
-              data={data}
-            />
-          </Card.Content>
-          <Card.Actions style={styles.cardActions}>
-            <Button
-              onPress={submitForm}
-              disabled={!isValid || isSubmitting || (isEditOperation && !dirty)}
-            >
-              Save
-            </Button>
-            <Button onPress={onDismiss}>Cancel</Button>
-          </Card.Actions>
-        </>
-      );
-    },
-    [onDismiss, formStarter, data, use, isEditOperation, setDirty]
+    (formik: FormikProps<MetadataFormInitialValues>) => (
+      <FormContents
+        formik={formik}
+        extra={{ formStarter, data, use, isEditOperation }}
+      />
+    ),
+    [formStarter, data, use, isEditOperation]
   );
 
   return (
