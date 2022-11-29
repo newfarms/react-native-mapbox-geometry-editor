607fab4a65ffcd6f515df71579db191541d31584
Bernard Llanos <bernard@mojow.ai>
2022-11-29 15:35:54 -0700

    Fix: fix FlatList rendering error
    - https://github.com/facebook/react-native/issues/29084

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,7 @@ module.exports = {
   plugins: [
     'react-native-paper/babel',
     ['@babel/plugin-proposal-decorators', { version: 'legacy' }],
+    ['@babel/plugin-transform-flow-strip-types'], // From https://github.com/facebook/react-native/issues/29084#issuecomment-1148014199
     ['@babel/plugin-proposal-class-properties'],
   ],
 };
--- example/babel.config.js
+++ example/babel.config.js
@@ -21,6 +21,7 @@ module.exports = function (api) {
         },
       ],
       ['@babel/plugin-proposal-decorators', { version: 'legacy' }],
+      ['@babel/plugin-transform-flow-strip-types'], // From https://github.com/facebook/react-native/issues/29084#issuecomment-1148014199
       ['@babel/plugin-proposal-class-properties'],
     ],
   };
